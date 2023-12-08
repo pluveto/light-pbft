e1a5634cb89753a36c6739f18d5a8e0305d285a3
pluveto <i@pluvet.com>
2023-12-08 15:27:56 +0800

    tests: Add util.ts file for testing and implement findPort and createSingleNodeConfig functions

---
+++ src/__tests__/util.ts
@@ -0,0 +1,46 @@
+import { SystemConfig } from '../config'
+import net from 'net'
+import { genKeyPair } from '../util'
+
+export async function findPort(): Promise<number> {
+    const nextPort = () => Math.floor(Math.random() * 10000) + 10000
+    const maxTries = 100
+    const portIsOccupied = (port: number) => {
+        const tester = net.createServer()
+        return new Promise((resolve) => {
+            tester.once('error', () => {
+                resolve(true)
+            })
+            tester.once('listening', () => {
+                tester.close()
+                resolve(false)
+            })
+            tester.listen(port)
+        })
+    }
+    for (let i = 0; i < maxTries; i++) {
+        const port = nextPort()
+        if (await portIsOccupied(port)) {
+            continue
+        }
+        return port
+    }
+
+    throw new Error('no available port')
+}
+
+export async function createSingleNodeConfig(name: string = 'node'): Promise<SystemConfig> {
+    return {
+        nodes: [
+            {
+                name: name,
+                host: 'localhost',
+                port: await findPort(),
+                ...genKeyPair(),
+            }
+        ],
+        params: {
+            f: 0
+        }
+    }
+}
